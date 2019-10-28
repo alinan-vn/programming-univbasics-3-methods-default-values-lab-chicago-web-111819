a146f0018481d4aacb7050ed6ad8b24f11ea7dc7
Alejandro Linan <jandrolinan@gmail.com>
2019-10-28 18:15:59 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,6 @@
 def meal_choice(veg1, veg2, protein = 'meat')
   statement1 = "What a nutritious meal!"
   statement2 = "A plate of #{protein} with #{veg1} and #{veg2}."
-  puts statement1
-  puts statement2
+  p statement1
+  p statement2
 end
